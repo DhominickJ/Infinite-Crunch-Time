1666856646df51bad0c47d6e758a20c3253f73c6
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2025-09-11 00:46:08 +0800

    Adding new code

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,10 +2,31 @@ extends TextureRect
 
 @onready var current_time = $HeaderMargin/Header/Time
 @onready var daysLeft_label = $HeaderMargin/Header/TurnsLeft
+@onready var art_button = $ArtButton
+@onready var music_button = $MusicButton
+@onready var programming_button = $ProgrammingButton
+
+# Background textures for different times of day
+@onready var morning_texture = preload("res://assets/objects/citymorning.png")
+@onready var noon_texture = preload("res://assets/objects/citynoon.png")
+@onready var night_texture = preload("res://assets/objects/citynight.png")
 
 func _ready() -> void:
-	
+	setup_button_animations()
+	update_background()  # Set initial background
 	pass
+
+func setup_button_animations():
+	# Start bounce animations for all buttons
+	bounce_button(art_button)
+	bounce_button(music_button)
+	bounce_button(programming_button)
+
+func bounce_button(button: Control):
+	var tween = create_tween()
+	tween.set_loops()
+	tween.tween_property(button, "position:y", button.position.y - 10, 0.5).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(button, "position:y", button.position.y, 0.5).set_trans(Tween.TRANS_SINE)
 
 func _physics_process(delta):
 #	GAME OVER IF THE NUMBER OF DAYS IS 0 AND TIME IS 6PM
@@ -37,14 +58,29 @@ func _on_music_button_pressed() -> void:
 #	LOGIC FOR DISPLAYING THE CURRENT TIME
 func display_time():
 	daysLeft_label.text = str("    Days Left: ", GlobalConfig.days_left)
-	
+
 	if GlobalConfig.current_time < 12:
 		current_time.text = str(GlobalConfig.current_time) + ":00AM     "
 	else:
 		current_time.text = str(GlobalConfig.current_time - 12) + ":00PM     "
+
+	# Update background based on time of day
+	update_background()
 
 	# Check if we passed the end of the day (e.g., after 6PM)
 	if GlobalConfig.current_time >= 18:
 		GlobalConfig.reset_day()
 		GlobalConfig.days_left -= 1
 		current_time.text = str(GlobalConfig.current_time) + ":00AM     "
+
+# FUNCTION TO UPDATE BACKGROUND BASED ON TIME OF DAY
+func update_background():
+	if GlobalConfig.current_time >= 6 and GlobalConfig.current_time < 12:
+		# Morning (6AM - 12PM)
+		texture = morning_texture
+	elif GlobalConfig.current_time >= 12 and GlobalConfig.current_time < 18:
+		# Noon/Afternoon (12PM - 6PM)
+		texture = noon_texture
+	else:
+		# Night (6PM - 6AM)
+		texture = night_texture
